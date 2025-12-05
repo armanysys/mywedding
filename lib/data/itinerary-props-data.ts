f59a2236278fc15db00fbf4721705c2558d87c61
Armando Antonio <32909362+armanysys@users.noreply.github.com>
2025-12-04 21:07:32 -0600

    009 Refactor itinerary

--- lib/data/itinerary-props-data.ts
+++
@@ -1,6 +0,0 @@
-import type { ItineraryProps } from "@/lib/interfaces/ItineraryProps"
-
-export const itineraryProps: ItineraryProps = {
-  Title: "Itinerario del Día",
-  Description: "Un día lleno de amor, alegría y celebración",
-}
--- lib/interfaces/ItineraryProps.ts
+++ lib/interfaces/ItineraryProps.ts
@@ -1,4 +1,13 @@
 export interface ItineraryProps {
     Title: string
-    Description: string
+    Description: string,
+    ScheduleItem: ScheduleItem[]
 }
+
+export interface ScheduleItem {
+    time: string
+    title: string
+    description: string
+    icon: string
+}
+
